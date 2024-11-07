180a62705be04ea08d4a759aa6f44df9839d505a
Comfort Ngwenya <>
2024-11-07 10:28:38 +0200

    Deleting the not needed fiels and reset my project for a new App simple structure. and created the birthday card component

---
+++ app/index.tsx
@@ -0,0 +1,28 @@
+import { Text, View, StyleSheet } from "react-native";
+import BirthdayCard from '../components/BirthdayCard';
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View style ={{ flex: 1}}>
+      <BirthdayCard />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  text: {
+    color: 'white'
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: "#fff",
+  }
+})
