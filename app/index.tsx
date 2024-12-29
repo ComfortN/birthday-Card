79a0529dd3c3e454c1e2ba15b5d1be5ee2ec0ea5
Comfort Ngwenya <nqobilecomfyngwenya@gmail.com>
2024-12-30 00:00:41 +0200

    Updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
-import React = require("react");
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useFonts } from 'expo-font';
